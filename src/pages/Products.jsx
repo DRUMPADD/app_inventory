eb02da061eab3218afb29a7253ee329c90295286
Erick Patiño <erickfco1999@gmail.com>
2025-01-19 09:31:52 -0600

    Bug from styles files fixed

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,4 +1,5 @@
 import {useEffect, useState} from 'react'
+import '@/styles/Products.css';
 import ProductsCard from "@/components/ProductsCard";
 import SearchBar from "@/components/SearchBar";
 import useSearchProduct from "@/hooks/useSearchProduct";
